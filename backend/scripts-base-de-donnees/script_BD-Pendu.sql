18e87ba801dcfb6867e0dd903e281b36977d0763
Martinp-95 <m.perdaens@students.ephec.be>
2020-04-03 10:10:55 +0200

    ajout des scrip de la DB

---
+++ backend/scripts-base-de-donnees/script_BD-Pendu.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE `mots` (
+    `languesId` varchar(2)  NOT NULL ,
+    `mot` varchar(50)  NOT NULL ,
+    PRIMARY KEY (`languesId`, `mot`)
+);
+
+CREATE TABLE `langues` (
+    `languesId` varchar(2) NOT NULL ,
+    `nom` varchar(30)  NOT NULL, 
+     PRIMARY KEY (`languesId`)
+);
+
+CREATE TABLE `classement` (
+    `userId` int  NOT NULL ,
+    `score` int  NOT NULL ,
+    PRIMARY KEY (
+        `userId`
+    )
+);
+
+CREATE TABLE `utilisateurs` (
+    `userId` int DEFAULT AUTOINCREMENT NOT NULL ,
+    `nom` varchar(50)  NOT NULL ,
+    `prenom` varchar(50)  NOT NULL ,
+    `pseudo` varchar(50)  NOT NULL ,
+    `mdp` varchar(100)  NOT NULL ,
+    PRIMARY KEY (
+        `userId`
+    )
+);
+
+ALTER TABLE `mots` ADD CONSTRAINT `fk_mots_languesId` FOREIGN KEY(`languesId`)
+REFERENCES `langues` (`languesId`);
+
+ALTER TABLE `classement` ADD CONSTRAINT `fk_classement_userId` FOREIGN KEY(`userId`)
+REFERENCES `utilisateurs` (`userId`);
