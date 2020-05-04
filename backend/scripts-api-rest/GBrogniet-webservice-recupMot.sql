c7a95c84e1e9168e7d9646d48da1ae75d01c1af1
BrognietGeoffrey <58220770+BrognietGeoffrey@users.noreply.github.com>
2020-05-04 11:27:22 +0200

    Update GBrogniet-webservice-recupMot.sql

--- backend/scripts-api-rest/GBrogniet-webservice-recupMot.sql
+++ backend/scripts-api-rest/GBrogniet-webservice-recupMot.sql
@@ -10,4 +10,4 @@ BEGIN
 END
 
 -------------------------------------------------------------------------------------------------
-CREATE SERVICE "recupMot" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call DBA.proc_recupMot(ln = languesId);
+CREATE SERVICE "recupMot" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call DBA.proc_recupMot(ln = languesIdIn);
