48378a67dd0c0c481753825a2647b4449bea145c
maxime <maxime.de.cock98@gmail.com>
2020-05-09 10:55:03 +0200

    update
    rafraichissement du tableau score à chaque findemot()

--- frontend/js/MDeCock-traitement-classement.js
+++ frontend/js/MDeCock-traitement-classement.js
@@ -2,28 +2,32 @@
 	
 	/*auteur: maxime de cock HE201554 */
 
-	
-	let xhr = new XMLHttpRequest();
-	
-	xhr.open('get', 'classement', true);
-	xhr.onload = 
-		function(){
-		
-		creerTable(JSON.parse(this.responseText), 'table_jeu');
-		}
-	xhr.send();
-	
-	function creerTable(reponseRequete, idBodyTable){
+	if (finDeMot()) {
+		let xhr = new XMLHttpRequest();
 
-		let table = '<tbody><tr><th> Nom du joueur </th><th> Score du joueur </th></tr>' ;
-		
-		for (let e of reponseRequete){
-			table += '<tr><td>' + e.pseudo + '</td><td>' + e.score + '</td></tr>';  
-		}
-		table += "</tbody>"
+		xhr.open('get', 'classement', true);
+		xhr.onload =
+			function () {
+				let table_jeu = 'table_jeu';
+				creerTable(JSON.parse(this.responseText), table_jeu);
+			};
+		xhr.send();
 	}
-	
-	document.getElementById(idBodyTable).innerHTML = table;
-	
-	
-	
+
+function creerTable(reponseRequete, idBodyTable) {
+
+			let table = '<tbody><tr><th> Nom du joueur </th><th> Score du joueur </th></tr>';
+
+			for (let e of reponseRequete) {
+				table += '<tr><td>' + e.pseudo + '</td><td>' + e.score + '</td></tr>';
+			}
+			table += "</tbody>";
+			document.getElementById(idBodyTable).innerHTML = table ;
+		}
+	}
+	console.log(incrementerScore());
+
+ function incrementerScore(id, score){
+
+
+}
