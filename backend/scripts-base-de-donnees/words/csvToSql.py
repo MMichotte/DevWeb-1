33dabbafc15bcd3607386d3b1fee290164e3a173
MMichotte <martin.michotte@gmail.com>
2020-05-10 12:04:37 +0200

    ajout des mots DE

--- backend/scripts-base-de-donnees/words/csvToSql.py
+++ backend/scripts-base-de-donnees/words/csvToSql.py
@@ -8,12 +8,26 @@ def convert(lang):
     with open("csv/"+lang+".csv") as f:
         csv_reader = csv.reader(f, delimiter=';')
         f = open("sql/insert_words_"+lang+".sql", "a")
+        count = 0
+        words = []
         for row in csv_reader:
-            word = row[1]
-            if len(word) > 5:
-                f.write(",\n('" + lang + "', '" + word + "')")
+            word = row[1].lower()
+            flag = True
+            for w in words :
+                if w == word :
+                    flag = False
+                    break
+            
+            if len(word) > 5 and not ("ï¿½" in word) and flag :
+                if count == 0 :
+                    f.write("('" + lang + "', '" + word + "')")
+                else :
+                    f.write(",\n('" + lang + "', '" + word + "')")
+                words.append(word)
+                count += 1
         f.write(";")
         f.close()
                 
-convert("en")
-convert("fr")
+#convert("en")
+#convert("fr")
+convert("de")
